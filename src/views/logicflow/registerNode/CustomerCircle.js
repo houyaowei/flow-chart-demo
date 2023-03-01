286c19bbbae997ba8467cdab34259e4ee0d36cfe
houyw <houyaowei@163.com>
2023-03-01 11:23:38 +0800

    feat：增加节点连接校验

--- src/views/logicflow/registerNode/CustomerCircle.js
+++ src/views/logicflow/registerNode/CustomerCircle.js
@@ -1,3 +1,8 @@
+/**
+ * 连线规则：
+ * 增加前置规则，需要实现getConnectedSourceRules
+ * 增加后置规则，需要实现getConnectedTargetRules
+ */
 import { CircleNode, CircleNodeModel } from "@logicflow/core";
 
 class CustomerCircleNode extends CircleNode {
@@ -7,8 +12,25 @@ class CustomerCircleNode extends CircleNode {
 class CustomerCircleModel extends CircleNodeModel{
     initNodeData(data) {
         super.initNodeData(data);
-        
     }
+    // getConnectedSourceRules() {
+
+    // }
+    // getConnectedTargetRules() {
+    //     const rules = super.getConnectedTargetRules();
+    //     const geteWayOnlyAsTarget = {
+    //       message: "上一个节点只能是开始节点",
+    //       validate: (sourceNode, targetNode, sourceAnchor, targetAnchor) => {
+    //         console.log("sourceNode:",sourceNode)
+    //         console.log("targetNode:",targetNode)
+    //         console.log("sourceAnchor:",sourceAnchor)
+    //         console.log("targetAnchor:",targetAnchor)
+    //         return false;
+    //       }
+    //     };
+    //     rules.push(geteWayOnlyAsTarget);
+    //     return rules;
+    // }
     setAttributes(){
         this.extraAttribute = "circle"
     }
